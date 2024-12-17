f78490e34be432e914d8e5151560a71a43240bbc
Hashidgaba <hashidgaba@gmail.com>
2024-12-17 22:58:40 +0500

    done by hashid gaba

--- script.js
+++ script.js
@@ -35,6 +35,15 @@ if (isLoggedIn) {
     });
 }
 
+// Hamburger Menu Toggle
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+
+
 // Function to display the list of voted societies and candidates
 function displayVoterList(username) {
     const votedDetails = JSON.parse(localStorage.getItem('votedDetails')) || [];
