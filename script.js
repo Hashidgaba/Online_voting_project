2641974f973ff45457617697f8870b062faff6bf
Hashidgaba <hashidgaba@gmail.com>
2024-12-22 00:25:51 +0500

    Done

--- script.js
+++ script.js
@@ -36,11 +36,22 @@ if (isLoggedIn) {
 }
 
 // Hamburger Menu Toggle
-const hamburger = document.getElementById("hamburger");
+const hamburger = document.querySelector(".hamburger");
 const navMenu = document.getElementById("navMenu");
 
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
+    if(navMenu.classList.contains("active")){
+        hide.forEach(hide => {
+            hide.style.display = 'none'
+        })
+
+    }else{
+
+        hide.forEach(hide => {
+            hide.style.display = 'block'
+        })
+    }
 });
 
 
